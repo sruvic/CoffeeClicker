b05cea0c2540f384129c88c0f0e9316cda90daf0
sruvic <sruvic5@gmail.com>
2023-03-13 23:10:35 +0100

    Version 0.0.1

---
+++ CC/main.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" href = "../css/index.css">
+    <title>Coffee Clicker</title>
+</head>
+<body>
+    <div class = "Header">
+    <h1 class = "Welcome">Welcome to a world of coffee</h1>
+    </div>
+    <div class = "wbody">
+
+    <div class = "Buttons">
+        <div class = "Menu">
+        <b><a class = "Home" href = "#">Home<a>
+        <b><a class = "Forum" href = "#">Forum<a>
+        <b><a class = "Download" href = "#">Download<a>
+</div>
+    </div>
+
+    </div>
+
+     
+     <script src = "../CC/js/index.js"></script>
+</body>
+</html>
